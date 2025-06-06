96fc92d04b671f28fc742d904444c48390b5de5b
alexdlct <112514808+alexdlct@users.noreply.github.com>
2025-06-06 16:41:45 -0700

    unrefactoring

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -11,61 +11,6 @@
 #include "filesys/filesys.h"
 #include "userprog/pagedir.h"
 
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!! fd stuff !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-
-struct fd_entry *fd_create(struct file *f) {
-  if(f == NULL){
-      return NULL;
-  }
-  struct thread *cur = thread_current();
-  struct fd_entry *fde = malloc(sizeof(struct fd_entry));
-  if (!fde) return NULL;
-  
-  fde->fd = cur->next_fd_num++;
-  fde->file = f;
-  list_push_back(&cur->fd_list, &fde->elem);
-  return fde;
-}
-
-struct fd_entry *fd_lookup(int fd) {
-  struct list *fds = &thread_current()->fd_list;
-  struct list_elem *e;
-  for (e = list_begin(fds); e != list_end(fds); e = list_next(e)) {
-      struct fd_entry *fde = list_entry(e, struct fd_entry, elem);
-      if (fde->fd == fd)
-          return fde;
-  }
-  return NULL;
-}
-
-bool fd_close(int fd) {
-  struct list *fds = &thread_current()->fd_list;
-  struct list_elem *e;
-
-  for (e = list_begin(fds); e != list_end(fds); e = list_next(e)) {
-      struct fd_entry *fde = list_entry(e, struct fd_entry, elem);
-      if (fde->fd == fd) {
-          file_close(fde->file);               // Close the file
-          list_remove(&fde->elem);             // Remove from fd_list
-          free(fde);                           // Free the struct
-          return true;
-      }
-  }
-
-  return false;  // fd not found
-}
-
-void fd_close_all(void) {
-  struct list *fds = &thread_current()->fd_list;
-  while (!list_empty(fds)) {
-      struct list_elem *e = list_pop_front(fds);
-      struct fd_entry *fde = list_entry(e, struct fd_entry, elem);
-      file_close(fde->file);
-      free(fde);
-  }
-}
-
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!! end !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 static struct lock filesys_lock;
 
 static void syscall_handler(struct intr_frame *);
